ff3655d5e5be5001c2b65eeff6dc6ed11b4eb518
thirasak <nayzerotv@gmail.com>
2023-03-05 01:11:44 +0700

    Add GUI & Create Gameplay and Game Menu Scenes

---
+++ Assets/Scripts/Game Menu/MenuManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public Image menu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            // The "Enter" key has been pressed
+            // Do something here, like print a message
+            Debug.Log("Start Game");
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int score;
-    private bool _isGameActive = true;
+    private bool _isGameActive = false;
     private bool _isGameOver = false;
 
+    public GameObject player;
+    public Image menu;
+    public Image gameOver;
+    public TextMeshProUGUI lastScore;
 
     public bool isGameOver
     {
@@ -21,16 +28,34 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isGameActive && !isGameOver &&Input.GetKeyDown(KeyCode.Return))
+        {
+            // The "Enter" key has been pressed
+            // Do something here, like print a message
+            Debug.Log("Game Over");
+            GameOver();
+        }
+
+        if (isGameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                // The "Enter" key has been pressed
+                // Do something here, like print a message
+                Debug.Log("Restart Game");
+                RestartGame();
+            }
+        }
     }
 
-    public void StartGame(int difficulty)
+    public void StartGame()
     {
         _isGameActive = true;
         score = 0;
@@ -39,12 +64,25 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        
         Debug.Log("Game Over!  -- Score = "+ score);
+        
         _isGameActive = false;
         _isGameOver = true;
-        
+        lastScore.text = "Score: " + score;
+        gameOver.gameObject.SetActive(true);
+        Debug.Log("GameOver isGameActive = "+_isGameActive);
+
+
     }
 
+    public void RestartGame()
+    {
+        // Reset the game state to the beginning
+        gameOver.gameObject.SetActive(false);
+        SceneManager.LoadScene("Gameplay");
+
+    }
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
